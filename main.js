dada3a43a2bfec9660c7b9de9f0fa28a6ec64bcf
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-08-21 13:28:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ const mention = (user) => {
 
 const escapeMarkdown = (text) => {
     return text
-        .replace(/([_*`~\\\[\]()#\-])/g, '\\$1');
+        .replace(/([_*`~\\\[\]()#\-\|])/g, '\\$1');
 };
 
 const formatText = (text) => {
