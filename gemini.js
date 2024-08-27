c3fc52d9a7cee62ef01275e7e22c58d9377bc857
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-08-27 22:10:09 +0700

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
+        winston.format.colorize(),
         winston.format.timestamp(),
         winston.format.printf(({ timestamp, level, message }) => {
             return `${timestamp} [${level.toUpperCase()}]: ${message}`;
